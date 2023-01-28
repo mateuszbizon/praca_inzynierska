21bbc42f7965f5f272f86390bb90bcede63c25e2
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-01-28 20:19:06 +0100

    changed setting values of inputs in login page

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { configureStore, applyMiddleware, componse } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import reducers from "./reducers";
 
 import App from "./App";
 
-const store = configureStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- client/src/routes/login.js
+++ client/src/routes/login.js
@@ -1,74 +1,76 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../sass/css/login.css";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import HomeNavbar from "../components/homeNavbar";
 
-const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const initialState = { email: "", password: "" };
 
 function Login() {
-	const emailRef = useRef();
-	const passRef = useRef();
-    const emailError = useRef();
-    const passError = useRef();
+	const [form, setForm] = useState(initialState);
+	const emailError = useRef();
+	const passError = useRef();
 
-    const showPass = useRef();
-    const hidePass = useRef();
-	
-    function checkEmail(){
-        let email = emailRef.current.value;
+	const showPass = useRef();
+	const hidePass = useRef();
 
-        if(email.length == 0){
-            emailError.current.textContent = 'Email nie może być pusty';
-			emailError.current.style.visibility = 'visible';
-            return false;
-        }
+	function checkEmail() {
+		const emailRegex =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        if(!email.match(emailRegex)){
-            emailError.current.textContent = 'Nieprawidłowy email';
-			emailError.current.style.visibility = 'visible';
-            return false;
-        }
-
-        emailError.current.textContent = '';
-		emailError.current.style.visibility = 'hidden';
-        return true;
-    }
-
-	function checkPassword(){
-		let password = passRef.current.value;
-
-		if(password.length == 0){
-            passError.current.textContent = 'Hasło nie może być puste';
-			passError.current.style.visibility = 'visible';
-            return false;
-        }
-
-		passError.current.textContent = '';
-		passError.current.style.visibility = 'hidden';
-		return true;
-	}
-
-	function showPassword(){
-		if(passRef.current.type === 'password'){
-			passRef.current.type = 'text';
-			showPass.current.style.display = 'block';
-			hidePass.current.style.display = 'none';
-		} else {
-			passRef.current.type = 'password';
-			showPass.current.style.display = 'none';
-			hidePass.current.style.display = 'block';
-		}
-	}
-
-	function handleSubmit(e){
-		e.preventDefault();
-
-		if(!checkEmail() || !checkPassword()){
+		if (form.email.length == 0) {
+			emailError.current.textContent = "Email nie może być pusty";
+			emailError.current.style.visibility = "visible";
 			return false;
 		}
 
-		console.log('submitted');
+		if (!form.email.match(emailRegex)) {
+			emailError.current.textContent = "Nieprawidłowy email";
+			emailError.current.style.visibility = "visible";
+			return false;
+		}
+
+		emailError.current.textContent = "";
+		emailError.current.style.visibility = "hidden";
+		return true;
+	}
+
+	function checkPassword() {
+		if (form.password.length == 0) {
+			passError.current.textContent = "Hasło nie może być puste";
+			passError.current.style.visibility = "visible";
+			return false;
+		}
+
+		passError.current.textContent = "";
+		passError.current.style.visibility = "hidden";
+		return true;
+	}
+
+	function showPassword() {
+		if (passRef.current.type === "password") {
+			passRef.current.type = "text";
+			showPass.current.style.display = "block";
+			hidePass.current.style.display = "none";
+		} else {
+			passRef.current.type = "password";
+			showPass.current.style.display = "none";
+			hidePass.current.style.display = "block";
+		}
+	}
+
+	function onChange(e) {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		if (!checkEmail() || !checkPassword()) {
+			return false;
+		}
+
+		console.log("submitted");
 	}
 	return (
 		<>
@@ -78,25 +80,52 @@ function Login() {
 					<h1 className='login__heading'>Logowanie</h1>
 					<form>
 						<div className='login__box'>
-							<input type='text' id='email' ref={emailRef} required/>
+							<input
+								type='text'
+								id='email'
+								name='email'
+								onChange={onChange}
+								required
+							/>
 							<label htmlFor='email'>Adres email</label>
-                            <p className="login__text-error" ref={emailError}>error</p>
+							<p className='login__text-error' ref={emailError}>
+								error
+							</p>
 						</div>
 						<div className='login__box'>
-							<input type='password' id='password' ref={passRef} required/>
+							<input
+								type='password'
+								id='password'
+								name='password'
+								onChange={onChange}
+								required
+							/>
 							<label htmlFor='password'>Hasło</label>
-							<span className="login__password-icons" >
-								<VisibilityOffIcon className="hide-password" ref={hidePass} onClick={showPassword}/>
-								<VisibilityIcon className="show-password" ref={showPass} onClick={showPassword}/>
+							<span className='login__password-icons'>
+								<VisibilityOffIcon
+									className='hide-password'
+									ref={hidePass}
+									onClick={showPassword}
+								/>
+								<VisibilityIcon
+									className='show-password'
+									ref={showPass}
+									onClick={showPassword}
+								/>
 							</span>
-							<p className="login__text-error" ref={passError}>error</p>
+							<p className='login__text-error' ref={passError}>
+								error
+							</p>
 						</div>
 						<div className='login__btn-box'>
-							<button type='submit' onClick={handleSubmit} className='login__submit'>
+							<button
+								type='submit'
+								onClick={handleSubmit}
+								className='login__submit'>
 								Zaloguj się
 							</button>
 						</div>
-						<div className="login__info-box">
+						<div className='login__info-box'>
 							<p className='login__info'>Nie masz konta?</p>
 							<a className='login__link' href='#'>
 								Zarejestruj się
