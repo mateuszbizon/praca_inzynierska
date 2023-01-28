e17f8be4067c839aee5599e46f7363b31f12fa81
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-01-28 18:51:48 +0100

    added dotenv file

--- server/index.js
+++ server/index.js
@@ -15,12 +15,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-	"mongodb+srv://socialspeedcubing:socialspeedcubing123@cluster0.tkldegl.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() =>
 		app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
 	)
