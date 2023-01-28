23a15a05c8f11682b8b4899ae316956aabb1e8ca
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-01-28 18:45:33 +0100

    deleted .env

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
@@ -15,12 +15,12 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-// const CONNECTION_URL =
-// 	"mongodb+srv://socialspeedcubing:socialspeedcubing123@cluster0.tkldegl.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL =
+	"mongodb+srv://socialspeedcubing:socialspeedcubing123@cluster0.tkldegl.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() =>
 		app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
 	)
