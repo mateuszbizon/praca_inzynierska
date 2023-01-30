2f195e076d2ac767e28ae06a199f188e4380d952
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-01-30 15:04:25 +0100

    added navbar and sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './routes/login';
 import RegisterPage from './routes/Register';
+import HomePage from './routes/Home';
 
 function App() {
   return (
 		<Router>
 			<Routes>
+				<Route path='/' element={<HomePage />} />
 				<Route path='/login' element={<LoginPage />} />
 				<Route path='/register' element={<RegisterPage />} />
 			</Routes>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,84 @@
-import React from 'react';
-import '../sass/css/homeNavbar.css';
-import logo from '../img/logo.png';
+import React, { useState } from "react";
+import "../sass/css/navbar.css";
+import logo from "../img/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from '@mui/icons-material/Edit';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function Navbar() {
-  return (
-    <>
-        <nav className='navbar'>
-            <a href="/" title="Strona główna">
-                <img src={logo} alt="asdasads" className='navbar__logo' />
-            </a>
-            <div className="navbar__elements">
-                <a href="/login" className='navbar__link'>Logowanie</a>
-                <a href="/register" className='navbar__link'>Rejestracja</a>
+  const [sidebar, setSidebar] = useState(false);
+
+  function showSidebar(){
+    setSidebar(!sidebar)
+  }
+	return (
+		<>
+			<nav className='navbar'>
+				<a href='/' title='Strona główna'>
+					<img src={logo} alt='Kostka Rubika' className='navbar__logo' />
+				</a>
+				<div className='navbar__burger'>
+					<MenuIcon fontSize='large' className='navbar__burger-icon' onClick={showSidebar}/>
+				</div>
+			</nav>
+
+			<nav className={sidebar ? 'sidebar active' : 'sidebar'}>
+				<div className="sidebar__row">
+          <div className='sidebar__items'>
+            <div className='sidebar__icons'>
+              <HomeIcon fontSize='large' className='sidebar__icon' />
             </div>
-        </nav>
-    </>
-  )
+            <div className="sidebar__text">
+              Strona główna
+            </div>
+          </div>
+        </div>
+				<div className="sidebar__row">
+          <div className='sidebar__items'>
+            <div className='sidebar__icons'>
+                <AddIcon fontSize='large' className='sidebar__icon' />
+            </div>
+            <div className="sidebar__text">
+              Utwórz
+            </div>
+          </div>
+        </div>
+				<div className="sidebar__row">
+          <div className='sidebar__items'>
+            <div className='sidebar__icons'>
+                <EditIcon fontSize='large' className='sidebar__icon' />
+            </div>
+            <div className="sidebar__text">
+            Edytuj profil
+            </div>
+          </div>
+        </div>
+				<div className="sidebar__row">
+          <div className='sidebar__items'>
+            <div className='sidebar__icons'>
+                <ChangeCircleIcon fontSize='large' className='sidebar__icon' />
+            </div>
+            <div className="sidebar__text">
+            Zmień hasło
+            </div>
+          </div>
+        </div>
+				<div className="sidebar__row">
+          <div className='sidebar__items'>
+            <div className='sidebar__icons'>
+                <LogoutIcon fontSize='large' className='sidebar__icon' />
+            </div>
+            <div className="sidebar__text">
+              Wyloguj się
+            </div>
+          </div>
+        </div>
+			</nav>
+		</>
+	);
 }
 
 export default Navbar;
