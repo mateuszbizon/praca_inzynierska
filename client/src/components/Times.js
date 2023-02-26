d24ac4167bde46f78fb3fde078e0c371baf010ec
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-02-26 18:08:23 +0100

    viewing reverse times and index in array

--- client/src/components/Times.js
+++ client/src/components/Times.js
@@ -8,9 +8,9 @@ function Times({ sessions, session }) {
             <div className="timer__results-time-number">Lp.</div>
             <div className="timer__results-time">Czas</div>
         </div>
-        {currentSession?.times.map((t, i) => (
+        {currentSession?.times.slice(0).reverse().map((t, i, a) => (
             <div key={i} className="timer__results-time-box">
-                <div className="timer__results-time-number">{i + 1}</div>
+                <div className="timer__results-time-number">{a.length - i}</div>
                 <div className="timer__results-time">{t}</div>
             </div>
         ))}
