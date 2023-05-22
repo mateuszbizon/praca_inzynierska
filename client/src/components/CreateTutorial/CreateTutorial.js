22fbb1bfe4cbbe937a8329a8019b14115f1d7420
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-05-22 14:59:13 +0200

    splitted creating posts and tutorials into 2 components

---
+++ client/src/components/CreateTutorial/CreateTutorial.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './create-tutorial.css';
+
+function CreateTutorial() {
+  return (
+    <div>Tworzenie tutoriala...</div>
+  )
+}
+
+export default CreateTutorial
--- client/src/routes/Create/Create.js
+++ client/src/routes/Create/Create.js
@@ -1,61 +1,37 @@
-import React, { useState, useRef, useEffect } from "react";
-import TextField from "@mui/material/TextField";
+import React, { useState, useRef } from "react";
+import CreatePost from "../../components/CreatePost/CreatePost";
+import CreateTutorial from "../../components/CreateTutorial/CreateTutorial";
 import "./create.css";
-import FileBase from 'react-file-base64';
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { createPost } from "../../actions/posts";
-import { CircularProgress } from "@mui/material";
-import createValid from "../../validations/CreateValid";
 
 function Create() {
-    const [form, setForm] = useState({ message: '', selectedFile: '' });
-    const [errors, seterrors] = useState({})
-    const dispatch = useDispatch();
-    const { isLoading } = useSelector(state => state.loaders)
-    const navigate = useNavigate();
-    const user = JSON.parse(localStorage.getItem("user"));
+    const [changeView, setChangeView] = useState("create-post");
+    const markerRef = useRef();
 
-    function onChange(e){
-        setForm({ ...form, [e.target.name]: e.target.value });
-    }
+    function indicator(e) {
+		markerRef.current.style.left = `${e.offsetLeft}px`;
+		markerRef.current.style.width = `${e.offsetWidth}px`;
+	}
 
-    function handleSubmit(e){
-        e.preventDefault();
-
-        seterrors(createValid(form))
-    }
-
-    useEffect(() => {
-        if (Object.keys(errors).length == 0 && form.message !== "" && form.selectedFile !== "") {
-            dispatch(createPost({ ...form, username: user.result.username}, navigate));
-        }
-    }, [errors])
+	function changeViewAndMarker(e, viewToChange) {
+		setChangeView(viewToChange);
+		indicator(e.target);
+	}
 
 	return (
 		<>
 			<section className='create'>
-				<div className='create__form'>
-                    <h1 className="create__heading">Utwórz post</h1>
-                    <form>
-                        <div className="create__form-box">
-                            <textarea id='message' name='message' onChange={onChange} required></textarea>
-                            <label htmlFor="message">Wiadomość</label>
-                            <p className={errors.message ? "create__text-error create__show-input-error" : "create__text-error"} >{errors.message ? errors.message : "error"}</p>
-                        </div>
-                        <div className="create__form-box">
-                            <FileBase name="selectedFile" type="file" multiple={false} onDone={({ base64 }) => setForm({ ...form, selectedFile: base64 })} />
-                            <p className={errors.selectedFile ? "create__text-error create__show-input-error" : "create__text-error"} >{errors.selectedFile ? errors.selectedFile : "error"}</p>
-                        </div>
-                        <div className='create__btn-box'>
-                                <button
-                                    type='submit'
-                                    onClick={handleSubmit}
-                                    className='create__submit'>
-                                    Utwórz {isLoading && <CircularProgress size="25px" style={{color: "#fff"}} />}
-                                </button>
-                        </div>
-                    </form>
+				<div className='create__container'>
+                    <div className='create__main-buttons'>
+                        <button onClick={e => changeViewAndMarker(e, "create-post")}>
+                            Utwórz post
+                        </button>
+                        <button onClick={e => changeViewAndMarker(e, "create-tutorial")}>
+                            Utwórz poradnik
+                        </button>
+                        <div className='create__marker' ref={markerRef}></div>
+                    </div>
+                   {changeView === "create-post" && <CreatePost />} 
+                   {changeView === "create-tutorial" && <CreateTutorial />} 
 				</div>
 			</section>
 		</>
