086d6cfd4cbec0b1de25ae267be663a1211de24c
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-06-11 18:06:19 +0200

    added loading to contest details

--- client/src/routes/ContestDetails/ContestDetails.js
+++ client/src/routes/ContestDetails/ContestDetails.js
@@ -6,12 +6,14 @@ import { getContestById } from '../../actions/contests'
 import RegisterUserToContest from '../../components/RegisterUserToContest/RegisterUserToContest'
 import ContestUsers from '../../components/ContestUsers/ContestUsers'
 import Marker from '../../components/Marker/Marker'
+import Loader from "../../components/Loader/Loader"
 
 function ContestDetails() {
     const [changeView, setChangeView] = useState("register")
     const { id } = useParams()
     const dispatch = useDispatch()
     const { contest } = useSelector(state => state.contests)
+    const { isLoading } = useSelector(state => state.loaders)
     const firstButtonRef = useRef()
     const [indicator, setIndicator] = useState(null)
 
@@ -26,43 +28,47 @@ function ContestDetails() {
 
   return (
     <section className='contest-details'>
-        <div className='contest-details__box'>
-            <div className='contest-details__box-item'>
-                <h3 className='contest-details__title'>Data</h3>
-                <p className='contest-details__text'>{new Date(contest.startContest).getDate()} - {new Date(contest.endContest).getDate()} {new Date(contest.startContest).toLocaleString('default', { month: 'long' })}, {new Date(contest.startContest).getFullYear()}</p>
-            </div>
-            <div className='contest-details__box-item'>
-                <h3 className='contest-details__title'>Miejsce</h3>
-                <p className='contest-details__text'>{contest.typeContest === "on-line" ? `zawody ${contest.typeContest}` : contest.place}</p>
-            </div>
-            <div className='contest-details__box-item'>
-                <h3 className='contest-details__title'>Początek rejestracji</h3>
-                <p className='contest-details__text'>{new Date(contest.startRegistration).getDate()} {new Date(contest.startRegistration).toLocaleString('default', { month: 'long' })} {new Date(contest.startRegistration).getFullYear()}, {new Date(contest.startRegistration).getHours()}:{new Date(contest.startRegistration).getMinutes()}</p>
-            </div>
-            <div className='contest-details__box-item'>
-                <h3 className='contest-details__title'>Koniec rejestracji</h3>
-                <p className='contest-details__text'>{new Date(contest.endRegistration).getDate()} {new Date(contest.endRegistration).toLocaleString('default', { month: 'long' })} {new Date(contest.endRegistration).getFullYear()}, {new Date(contest.endRegistration).getHours()}:{new Date(contest.endRegistration).getMinutes()}</p>
-            </div>
-            <div className='contest-details__box-item'>
-                <h3 className='contest-details__title'>Konkurencje</h3>
-                {contest.events?.map((event, index) => (
-                    <span className='contest-details__text' key={index}>{event.displayText}{index < contest.events.length - 1 && ","} </span>
-                ))}
-            </div>
-            <div className='contest-details__box-item'>
-                <h3 className='contest-details__title'>Limit zawodników</h3>
-                <p className='contest-details__text'>{contest.usersLimit}</p>
-            </div>
-        </div>
-        <div className='contest-details__change-view-btns'>
-            <button className='contest-details__change-view-btn' ref={firstButtonRef} onClick={e => changeViewAndMarker(e, "register")}>Rejestracja</button>
-            <button className='contest-details__change-view-btn' onClick={e => changeViewAndMarker(e, "users")}>Zawodnicy</button>
-            <button className='contest-details__change-view-btn' onClick={e => changeViewAndMarker(e, "regulations")}>Regulamin</button>
-            <button className='contest-details__change-view-btn' onClick={e => changeViewAndMarker(e, "live")}>Wyniki live</button>
-            <Marker firstButtonRef={firstButtonRef} indicator={indicator} />
-        </div>
-        {changeView === "register" && <RegisterUserToContest startRegistration={contest.startRegistration} endRegistration={contest.endRegistration} id={contest._id} />}
-        {changeView === "users" && <ContestUsers users={contest.users} events={contest.events} />}
+        {isLoading ? <Loader /> : (
+            <>
+                <div className='contest-details__box'>
+                    <div className='contest-details__box-item'>
+                        <h3 className='contest-details__title'>Data</h3>
+                        <p className='contest-details__text'>{new Date(contest.startContest).getDate()} - {new Date(contest.endContest).getDate()} {new Date(contest.startContest).toLocaleString('default', { month: 'long' })}, {new Date(contest.startContest).getFullYear()}</p>
+                    </div>
+                    <div className='contest-details__box-item'>
+                        <h3 className='contest-details__title'>Miejsce</h3>
+                        <p className='contest-details__text'>{contest.typeContest === "on-line" ? `zawody ${contest.typeContest}` : contest.place}</p>
+                    </div>
+                    <div className='contest-details__box-item'>
+                        <h3 className='contest-details__title'>Początek rejestracji</h3>
+                        <p className='contest-details__text'>{new Date(contest.startRegistration).getDate()} {new Date(contest.startRegistration).toLocaleString('default', { month: 'long' })} {new Date(contest.startRegistration).getFullYear()}, {new Date(contest.startRegistration).getHours()}:{new Date(contest.startRegistration).getMinutes()}</p>
+                    </div>
+                    <div className='contest-details__box-item'>
+                        <h3 className='contest-details__title'>Koniec rejestracji</h3>
+                        <p className='contest-details__text'>{new Date(contest.endRegistration).getDate()} {new Date(contest.endRegistration).toLocaleString('default', { month: 'long' })} {new Date(contest.endRegistration).getFullYear()}, {new Date(contest.endRegistration).getHours()}:{new Date(contest.endRegistration).getMinutes()}</p>
+                    </div>
+                    <div className='contest-details__box-item'>
+                        <h3 className='contest-details__title'>Konkurencje</h3>
+                        {contest.events?.map((event, index) => (
+                            <span className='contest-details__text' key={index}>{event.displayText}{index < contest.events.length - 1 && ","} </span>
+                        ))}
+                    </div>
+                    <div className='contest-details__box-item'>
+                        <h3 className='contest-details__title'>Limit zawodników</h3>
+                        <p className='contest-details__text'>{contest.usersLimit}</p>
+                    </div>
+                </div>
+                <div className='contest-details__change-view-btns'>
+                    <button className='contest-details__change-view-btn' ref={firstButtonRef} onClick={e => changeViewAndMarker(e, "register")}>Rejestracja</button>
+                    <button className='contest-details__change-view-btn' onClick={e => changeViewAndMarker(e, "users")}>Zawodnicy</button>
+                    <button className='contest-details__change-view-btn' onClick={e => changeViewAndMarker(e, "regulations")}>Regulamin</button>
+                    <button className='contest-details__change-view-btn' onClick={e => changeViewAndMarker(e, "live")}>Wyniki live</button>
+                    <Marker firstButtonRef={firstButtonRef} indicator={indicator} />
+                </div>
+                {changeView === "register" && <RegisterUserToContest startRegistration={contest.startRegistration} endRegistration={contest.endRegistration} id={contest._id} />}
+                {changeView === "users" && <ContestUsers users={contest.users} events={contest.events} />}
+            </>
+        )}
     </section>
   )
 }
