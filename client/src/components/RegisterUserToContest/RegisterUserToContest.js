619d92b437fc294ac37698d49bd9908cf97c8fe6
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-06-15 17:19:27 +0200

    added info for register user to contest

--- client/src/components/RegisterUserToContest/RegisterUserToContest.js
+++ client/src/components/RegisterUserToContest/RegisterUserToContest.js
@@ -19,7 +19,7 @@ function RegisterUserToContest({ startRegistration, endRegistration, id, events 
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault()    
 
     setErrors(registerUserContestValid(form))
   }
@@ -33,39 +33,44 @@ function RegisterUserToContest({ startRegistration, endRegistration, id, events 
   return (
     <>
       {Date.now() >= Date.parse(startRegistration) && Date.now() < Date.parse(endRegistration) && (
-        <div className='register-user-contest-form'>
-          <div className='register-user-contest-form__container'>
-            <h1 className='register-user-contest-form__heading'>Rejestracja na zawody</h1>
-            <form>
-              <div className='register-user-contest-form__box'>
-                <input className='register-user-contest-form__input' id="email" type='text' name="email" value={form.email} onChange={onChange} required/>
-                <label className='register-user-contest-form__label' htmlFor='email'>Adres email</label>
-                <p className={errors.email ? "register-user-contest-form__text-error register-user-contest-form__show-input-error" : "register-user-contest-form__text-error"} >
-                    {errors.email ? errors.email : "error"}
-                </p>
-              </div>
-              <div className='register-user-contest-form__box'>
-                <input className='register-user-contest-form__input' id="place" type='text' name="place" value={form.place} onChange={onChange} required/>
-                <label className='register-user-contest-form__label' htmlFor='place'>Miejscowość</label>
-                <p className={errors.place ? "register-user-contest-form__text-error register-user-contest-form__show-input-error" : "register-user-contest-form__text-error"} >
-                    {errors.place ? errors.place : "error"}
-                </p>
-              </div>
-              <div className='register-user-contest-form__box'>
-                <Select className='register-user-contest-form__select' placeholder="Wybierz konkurencje" isMulti options={events} onChange={item => setForm({ ...form, events: item })} closeMenuOnSelect={false} />
-                <p className={errors.events ? "register-user-contest-form__text-error register-user-contest-form__show-input-error" : "register-user-contest-form__text-error"} >
-                    {errors.events ? errors.events : "error"}
-                </p>
-              </div>
-              <p className={isBadRequest ? "register-user-contest-form__submit-message bad-color" : "register-user-contest-form__submit-message good-color"}>{isBadRequest ? badMessage : message}</p>
-              <div className='register-user-contest-form__btn-box'>
-                <button type='submit' onClick={handleSubmit} className='register-user-contest-form__submit-btn' disabled={isLoading}>
-                    Zarejestruj
-                </button>
-              </div>
-            </form>
+        <>
+          <div className='register-info'>
+            <p className='register-info__text'>Aby zarejestrować się w zawodach, musisz posiadać konto w naszym systemie. Jeśli nie masz konta <a className='register-info__link' href="/register" target='_blank'>zarejestruj się</a></p>
           </div>
-        </div>
+          <div className='register-user-contest-form'>
+            <div className='register-user-contest-form__container'>
+              <h1 className='register-user-contest-form__heading'>Rejestracja na zawody</h1>
+              <form>
+                <div className='register-user-contest-form__box'>
+                  <input className='register-user-contest-form__input' id="email" type='text' name="email" value={form.email} onChange={onChange} required/>
+                  <label className='register-user-contest-form__label' htmlFor='email'>Adres email</label>
+                  <p className={errors.email ? "register-user-contest-form__text-error register-user-contest-form__show-input-error" : "register-user-contest-form__text-error"} >
+                      {errors.email ? errors.email : "error"}
+                  </p>
+                </div>
+                <div className='register-user-contest-form__box'>
+                  <input className='register-user-contest-form__input' id="place" type='text' name="place" value={form.place} onChange={onChange} required/>
+                  <label className='register-user-contest-form__label' htmlFor='place'>Miejscowość</label>
+                  <p className={errors.place ? "register-user-contest-form__text-error register-user-contest-form__show-input-error" : "register-user-contest-form__text-error"} >
+                      {errors.place ? errors.place : "error"}
+                  </p>
+                </div>
+                <div className='register-user-contest-form__box'>
+                  <Select className='register-user-contest-form__select' placeholder="Wybierz konkurencje" isMulti options={events} onChange={item => setForm({ ...form, events: item })} closeMenuOnSelect={false} />
+                  <p className={errors.events ? "register-user-contest-form__text-error register-user-contest-form__show-input-error" : "register-user-contest-form__text-error"} >
+                      {errors.events ? errors.events : "error"}
+                  </p>
+                </div>
+                <p className={isBadRequest ? "register-user-contest-form__submit-message bad-color" : "register-user-contest-form__submit-message good-color"}>{isBadRequest ? badMessage : message}</p>
+                <div className='register-user-contest-form__btn-box'>
+                  <button type='submit' onClick={handleSubmit} className='register-user-contest-form__submit-btn' disabled={isLoading}>
+                      Zarejestruj
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </>
       )}
 
       {Date.now() < Date.parse(startRegistration) && (
