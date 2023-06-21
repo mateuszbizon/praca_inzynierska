ba685d4dc4f9ca1e3a57e0a59da1b391cbeb0a73
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-06-21 15:11:40 +0200

    deleted unused cases for users reducers

--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -7,12 +7,6 @@ const users = (state = { users: [] }, action) => {
 
         case actionType.GET_USERS_BY_SEARCH:
             return { ...state, users: action.data }
-
-        // case actionType.GET_USER_SESSIONS:
-        //     return { ...state, users: action.data }
-
-        // case actionType.ADD_NEW_TIME:
-        //     return { ...state, users: action.data }
     
         default:
             return state;
