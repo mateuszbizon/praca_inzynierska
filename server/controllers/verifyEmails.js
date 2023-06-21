309c8b84f4897509da9912df139d826f18a15f03
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-06-21 11:51:17 +0200

    added swagger documentation to verifyEmails route

--- server/controllers/verifyEmails.js
+++ server/controllers/verifyEmails.js
@@ -1,17 +1,16 @@
 import User from "../models/user.js";
 import Token from "../models/token.js";
+import mongoose from "mongoose";
 
 export const verifyRegisterEmail = async (req, res) => {
     const { id, token } = req.params
 
     try {
-        const user = await User.findOne({ _id: id })
-
-        if (!user) return res.status(400).json({ success: false, message: "Niepoprawny link"})
+        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ success: false, message: "Niepoprawny link" });
 
         const existingToken = await Token.findOne({ userId: id, token: token })
 
-        if (!existingToken) return res.status(400).json({ success: false, message: "Niepoprawny link"})
+        if (!existingToken) return res.status(400).json({ success: false, message: "Niepoprawny link" })
 
         await User.updateOne(({ _id: id, verified: true }))
         await existingToken.remove()
--- server/routes/verifyEmails.js
+++ server/routes/verifyEmails.js
@@ -3,6 +3,34 @@ import { verifyRegisterEmail } from '../controllers/verifyEmails.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: verifyEmails
+ */
+
+/**
+ * @swagger
+ * /emails/users/{id}/verify/{token}:
+ *   get:
+ *     description: Sprawdzanie czy użytkownik potwierdził rejestrację przez email
+ *     tags: [verifyEmails]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         type: string
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Rejestracja potwierdzona
+ *       400:
+ *         description: Niepoprawny link
+ */
+
 router.get("/users/:id/verify/:token", verifyRegisterEmail);
 
 export default router;
