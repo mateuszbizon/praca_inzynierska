12f605996df01039f70ed6053eed5a8478eea173
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-06-24 18:26:53 +0200

    changed marker position when window is resizing

--- client/src/components/Marker/Marker.js
+++ client/src/components/Marker/Marker.js
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import "./marker.css"
 
 function Marker({ firstButtonRef, indicator }) {
     const markerRef = useRef()
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
     function setMarkerFirst() {
         markerRef.current.style.left = `${firstButtonRef.current.offsetLeft}px`
@@ -17,8 +18,22 @@ function Marker({ firstButtonRef, indicator }) {
         if (indicator !== null) {
             markerRef.current.style.left = `${indicator.offsetLeft}px`;
             markerRef.current.style.width = `${indicator.offsetWidth}px`;
+        } else {
+          setMarkerFirst()
         }
-    }, [indicator])
+    }, [indicator, windowWidth])
+
+    useEffect(() => {
+      function handleWindowResize() {
+        setWindowWidth(window.innerWidth);
+      }
+  
+      window.addEventListener('resize', handleWindowResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleWindowResize);
+      };
+    });
 
   return (
     <div className='marker' ref={markerRef}></div>
