4d98175ff9e3092c79294f97dd19e088a4c8be51
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-06-25 13:55:32 +0200

    deleted / from socket io connect url

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
 	cors: {
-		origin: "https://social-speed-cubing.netlify.app/",
+		origin: "https://social-speed-cubing.netlify.app",
 		methods: ["GET", "POST"],
 	},
 });
