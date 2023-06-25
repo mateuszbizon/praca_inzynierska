e74484c86f71c4e45447923f75ef59cf9dc3fb6b
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-06-25 13:45:11 +0200

    added deployed frontend url to backend

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://social-speed-cubing.netlify.app/",
 		methods: ["GET", "POST"],
 	},
 });
