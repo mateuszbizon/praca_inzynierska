1df6fd1606fc95e5b1bb4cdcd84c0458b879714e
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-01-10 15:49:04 +0100

    Added new origin to socketio server cors

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
 	cors: {
-		origin: "https://social-speed-cubing.netlify.app",
+		origin: ["https://social-speed-cubing.netlify.app", "http://localhost:3000"],
 		methods: ["GET", "POST"],
 	},
 });
